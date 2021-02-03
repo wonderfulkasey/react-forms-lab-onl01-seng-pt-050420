ee4c0fb9f176a9a086914d86b3de232fad1dacc4
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 19:30:40 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
   }
 
   render() {
