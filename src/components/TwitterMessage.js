4beb06fea71bb4c405b0eecdf758839ec8f6d9ab
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 19:31:10 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -22,7 +22,8 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.value}/>
+        <span> {this.state.remainingChars}</span>
       </div>
     );
   }
