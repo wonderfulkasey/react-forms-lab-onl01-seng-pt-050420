90814b0bdf242d5be0f908934be340cd0aa045ae
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 19:29:13 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,6 +5,25 @@ class LoginForm extends React.Component {
     super(props);
 
     this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.username != "" && this.state.password != "") {
+      this.props.handleLogin({
+        username: this.state.username, 
+        password: this.state.password
+      });
+    }
   }
 
   render() {
